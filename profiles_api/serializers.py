fb7c9e9337856254b7e8715e775a66c71a063a48
mohammedsalmanj <jsalman23@outlook.com>
2023-03-26 17:59:32 +0530

    profile feed

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -9,7 +9,7 @@ class HelloSerializer(serializers.Serializer):
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    """Serializers a user profile object"""
+    """Serializes a user profile object"""
 
     class Meta:
         model = models.UserProfile
@@ -39,3 +39,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
             instance.set_password(password)
 
         return super().update(instance, validated_data)
+
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
