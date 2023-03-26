1105b3d437d5220a92ab2d5c7091b2cdc891e211
mohammedsalmanj <jsalman23@outlook.com>
2023-03-26 20:47:13 +0530

    error fix3

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '!t2ak#ixerx8&(s6g)irs^dr(=pfb2uw_57nz$o(oq!ycy7e)g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = ['ec2-54-199-24-89.ap-northeast-1.compute.amazonaws.com','127.0.0.1']
+ALLOWED_HOSTS = [
+    'ec2-54-199-24-89.ap-northeast-1.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
